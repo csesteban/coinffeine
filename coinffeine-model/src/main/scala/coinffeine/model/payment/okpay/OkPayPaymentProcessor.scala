aa23cef4f9b49cceb0f3f981542874e495a99d8e
Sebastian Ortega <sortega@coinffeine.com>
2015-09-02 12:44:09 +0200

    Use different limits and step sizes per currency

--- coinffeine-model/src/main/scala/coinffeine/model/payment/okpay/OkPayPaymentProcessor.scala
+++ coinffeine-model/src/main/scala/coinffeine/model/payment/okpay/OkPayPaymentProcessor.scala
@@ -7,28 +7,84 @@ import coinffeine.model.payment.PaymentProcessor
 
 object OkPayPaymentProcessor extends PaymentProcessor {
 
-  val SupportedCurrencies: Set[FiatCurrency] = Set(Euro, UsDollar)
-  val MinFee = BigDecimal(0.01)
-  val MaxFee = BigDecimal(2.99)
+  private val FeeRate = BigDecimal(0.005)
+  private val MaxFees = Map[FiatCurrency, FiatAmount](
+    Euro -> Euro(2.99),
+    UsDollar -> UsDollar(2.99),
+    PoundSterling -> PoundSterling(2.99),
+    HongKongDollar -> HongKongDollar(20),
+    SwissFranc -> SwissFranc(2.99),
+    AustralianDollar -> AustralianDollar(2.99),
+    PolishZloty -> PolishZloty(12),
+    Yen -> Yen(300),
+    SwedishKrona -> SwedishKrona(25),
+    DenmarkKroner -> DenmarkKroner(20),
+    CanadianDollar -> CanadianDollar(2.99),
+    RussianRouble -> RussianRouble(100),
+    Koruny -> Koruny(80),
+    CroatianKuna -> CroatianKuna(20),
+    HungarianForint -> HungarianForint(1000),
+    NorwegianKrone -> NorwegianKrone(20),
+    NewZealandDollar -> NewZealandDollar(2.99),
+    RomanianNewLeu -> RomanianNewLeu(13),
+    TurkishLira -> TurkishLira(9),
+    SouthAfricanRand -> SouthAfricanRand(40),
+    YuanRenminbi -> YuanRenminbi(2.99)
+  )
+  private val StepSize = Map[FiatCurrency, FiatAmount](
+    Euro -> Euro(2),
+    UsDollar -> UsDollar(2),
+    PoundSterling -> PoundSterling(2),
+    HongKongDollar -> HongKongDollar(17.5),
+    SwissFranc -> SwissFranc(2),
+    AustralianDollar -> AustralianDollar(3.2),
+    PolishZloty -> PolishZloty(8.5),
+    Yen -> Yen(300),
+    SwedishKrona -> SwedishKrona(20),
+    DenmarkKroner -> DenmarkKroner(15),
+    CanadianDollar -> CanadianDollar(3),
+    RussianRouble -> RussianRouble(150),
+    Koruny -> Koruny(55),
+    CroatianKuna -> CroatianKuna(15),
+    HungarianForint -> HungarianForint(630),
+    NorwegianKrone -> NorwegianKrone(20),
+    NewZealandDollar -> NewZealandDollar(3.5),
+    RomanianNewLeu -> RomanianNewLeu(9),
+    TurkishLira -> TurkishLira(6.5),
+    SouthAfricanRand -> SouthAfricanRand(30),
+    PhilippinePeso -> PhilippinePeso(100),
+    SingaporeDollar -> SingaporeDollar(3),
+    MalaysianRinggit -> MalaysianRinggit(10),
+    TaiwanNewDollar -> TaiwanNewDollar(70),
+    IsraeliNewSheqel -> IsraeliNewSheqel(9),
+    MexicanPeso -> MexicanPeso(40),
+    YuanRenminbi -> YuanRenminbi(15),
+    NigerianNairas -> NigerianNairas(450)
+  )
 
   def calculateFee(amount: FiatAmount): FiatAmount = {
     requireSupported(amount.currency)
-    val num = implicitly[Numeric[BigDecimal]]
-    val baseFee = amount.value * 0.005
-    val fee = num.min(num.max(baseFee, MinFee), MaxFee)
-    roundUp(fee, amount.currency)
+    val baseFee = roundUp(amount.value * FeeRate, amount.currency)
+    capByMaxFee(baseFee.max(minFee(amount.currency)))
   }
 
   override def bestStepSize(currency: FiatCurrency): FiatAmount = {
     requireSupported(currency)
-    currency(2)
+    StepSize(currency)
   }
+
+  private def minFee(currency: FiatCurrency) = currency.smallestAmount
+
+  private def capByMaxFee(baseFee: FiatAmount) =
+    MaxFees.get(baseFee.currency).fold(baseFee) { maxFee =>
+      baseFee.min(maxFee)
+    }
 
   private def roundUp(amount: BigDecimal, currency: FiatCurrency): FiatAmount =
     currency.exactAmount(amount.setScale(currency.precision, RoundingMode.UP))
 
   private def requireSupported(currency: FiatCurrency): Unit = {
-    require(SupportedCurrencies.contains(currency),
-      s"$currency is not supported, use one of: ${SupportedCurrencies.mkString(", ")}")
+    require(FiatCurrency.supported.contains(currency),
+      s"$currency is not supported, use one of: ${FiatCurrency.supported.mkString(", ")}")
   }
 }
--- coinffeine-model/src/test/scala/coinffeine/model/payment/okpay/OkPayPaymentProcessorTest.scala
+++ coinffeine-model/src/test/scala/coinffeine/model/payment/okpay/OkPayPaymentProcessorTest.scala
@@ -12,24 +12,28 @@ class OkPayPaymentProcessorTest extends UnitTest {
     OkPayPaymentProcessor.calculateFee(50.USD) shouldBe 0.25.USD
   }
 
-  it should "have a minimum fee of 0.01" in {
+  it should "have a minimum fee equivalent to the precision" in {
     OkPayPaymentProcessor.calculateFee(1.EUR) shouldBe 0.01.EUR
     OkPayPaymentProcessor.calculateFee(1.USD) shouldBe 0.01.USD
+    OkPayPaymentProcessor.calculateFee(1.JPY) shouldBe 1.JPY
   }
 
-  it should "have a maximum fee of 2.99" in {
+  it should "have a maximum fee" in {
     OkPayPaymentProcessor.calculateFee(1000.EUR) shouldBe 2.99.EUR
     OkPayPaymentProcessor.calculateFee(1000.USD) shouldBe 2.99.USD
+    OkPayPaymentProcessor.calculateFee(100000.JPY) shouldBe 300.JPY
   }
 
   it should "round up fees to the currency precision" in {
     OkPayPaymentProcessor.calculateFee(3.EUR) shouldBe 0.02.EUR
     OkPayPaymentProcessor.calculateFee(3.USD) shouldBe 0.02.USD
+    OkPayPaymentProcessor.calculateFee(300.JPY) shouldBe 2.JPY
   }
 
   it should "compute the best step size" in {
     OkPayPaymentProcessor.bestStepSize(Euro) shouldBe 2.EUR
     OkPayPaymentProcessor.bestStepSize(UsDollar) shouldBe 2.USD
+    OkPayPaymentProcessor.bestStepSize(Yen) shouldBe 300.JPY
   }
 
   it should "reject unsupported currencies" in {
